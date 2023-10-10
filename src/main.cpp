61f01a8bdaa68fa9dcdc4a987c47f3acfff60628
bipsydev <bipsydev@gmail.com>
2023-10-10 10:16:10 -0500

    Switch to raylib-cpp instead of C raylib

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,60 @@
-
-#include "raylib.h"
+#include "raylib-cpp.hpp"
 
 #include <string>
+#include <iostream>
 
-int main(void)
+
+
+int main(int argc, char * argv[])
 {
-    const int WIDTH = 1280;
-    const int HEIGHT = 720;
-
-    InitWindow(WIDTH, HEIGHT, "raylib [core] example - basic window");
-
-    // Set our game to run at 60 frames-per-second
-    SetTargetFPS(60);
-
-    const int FONT_SIZE = 20;
-    const std::string text = "Congrats! You created your first window!";
-    const int length = MeasureText(text.c_str(), FONT_SIZE);
-
-    // Main game loop
-    while (!WindowShouldClose()) // Detect window close button or ESC key
+    //TODO process command-line arguments
+    //TODO show launcher window if command-line arguments don't fill all values
+    
+    try
     {
-        // Update
-        // TODO: Update your variables here
+        const int WIDTH = 1280;
+        const int HEIGHT = 720;
+        raylib::Window window{WIDTH, HEIGHT, "Raylib C++ - Basic Window!"};
 
-        // Draw
-        BeginDrawing();
+        raylib::Texture logo{"raylib_logo.png"};
 
-            ClearBackground(RAYWHITE);
+        const std::string MESSAGE = "Congrats! You created your first window!";
+        const int FONT_SIZE = 20;
+        raylib::Text text{raylib::Font{}, MESSAGE, FONT_SIZE, 0, LIGHTGRAY};
+        const raylib::Vector2 TEXT_SIZE = text.MeasureEx();
+        const raylib::Vector2 TEXT_POS = {static_cast<float>(WIDTH)/2.0f - TEXT_SIZE.GetX()/2.0f,
+                                            static_cast<float>(HEIGHT)/2.0f - TEXT_SIZE.GetY()/2.0f};
 
-            DrawText(text.c_str(),
-                     WIDTH/2 - length/2,
-                     HEIGHT/2 - FONT_SIZE/2,
-                     FONT_SIZE, LIGHTGRAY);
+        // Set our game to run at 60 frames-per-second
+        window.SetTargetFPS(60);
 
-        EndDrawing();
+        // Main game loop
+        while (!window.ShouldClose()) // Detect window close button or ESC key
+        {
+            // Update
+            // TODO: Update your variables here
+
+            // Draw
+            window.BeginDrawing();
+
+                window.ClearBackground(RAYWHITE);
+                text.Draw(TEXT_POS);
+
+
+            window.EndDrawing();
+        }
+
+        // De-Initialization
+        // Unload Resources before closing window
+        logo.Unload();
+        // Close window and OpenGL context
+        window.Close();
+
     }
-
-    // De-Initialization
-    // Close window and OpenGL context
-    CloseWindow();
+    catch(const raylib::RaylibException& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
     return 0;
 }
